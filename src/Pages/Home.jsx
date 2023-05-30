1f351edfa4c24c2442518c03ecb64cbd001cb796
Dev_Yoshi <fujitaniyoshilynp@gmail.com>
2023-05-30 19:56:16 +0800

    Minor Changes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -16,6 +16,13 @@ const Home = () => {
             <img src="../src/assets/toyota3.png" alt="" />
         </div>
       </div>
+    {/* First Sub Hero */}
+      <div className="">
+        <div className="">
+
+        </div>
+        <h1 className='font-extrabold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>Lowest Possible Price in Town</h1>
+      </div>
     </div>
   )
 }
